120b8a91a74cd55f2b55eb71d3964fb8a59b0724
ermolaeva1 <ermolaeva1@209-8>
2025-09-29 14:46:10 +0200

    Добавьте файлы проекта.

---
+++ arifmetich virazh/Program.cs
@@ -0,0 +1,13 @@
+﻿//Лабараторная работа #1
+//Арифметические выражения
+//Вариант 25. Высокий
+Console.Write("введите a");
+double a =double.Parse(Console.ReadLine());
+Console.Write("введите y");
+double y = double.Parse(Console.ReadLine());
+Console.Write("введите x");
+double x = double.Parse(Console.ReadLine());
+
+double b = (Math.Pow(1 / Math.Tan(Math.Pow(a, 3) * Math.PI / 180), 3));
+double c = (Math.Pow(Math.Atan(a), 2) * Math.PI / 180);
+Console.WriteLine($"J={(b+c) / (Math.Sqrt(Math.Pow(y, Math.Tan(x)))):f2}");
